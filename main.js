e402a1cea849e23d9987aa008e88d7f221a632a3
Fadi Ayad <feayad@gmail.com>
2019-10-28 21:00:55 -0500

    working on adding a navbar to move around the page

--- main.js
+++ main.js
@@ -15,5 +15,4 @@ mybutton = document.getElementById("topButton");
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
-
+}
